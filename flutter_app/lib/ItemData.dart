3e284eae82eb8cc9c51721e1e1003ef6d2f06353
sixiaobing <sixiaobing@9fbank.cc>
2019-06-14 17:55:32 +0800

    路由界面练习二

--- flutter_app/lib/ItemData.dart
+++ flutter_app/lib/ItemData.dart
@@ -4,15 +4,17 @@ class ItemDatas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
+      color: Colors.amber,
       height: 80,
-      padding: EdgeInsets.all(10),
       child: new Container(
-        color: Colors.amber,
+        padding: EdgeInsets.only(left: 100,top: 0,right: 0,bottom: 0),
 
         child: new Row(
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
             Text('数据元素'),
+            Text('间隔',style: TextStyle(fontSize: 20),),
             Icon(Icons.access_alarms),
           ],
         ),
